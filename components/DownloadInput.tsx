7e5c1d0d4dcba93157291905b0513af33ef15abe
RiinaVi <riinavi86@gmail.com>
2020-03-27 13:42:16 +0200

    "fix upload 2.0"

--- components/DownloadInput.tsx
+++ components/DownloadInput.tsx
@@ -1,6 +1,5 @@
 import React, {useRef, useState} from "react";
 import {NextPage} from "next";
-import {request} from "http";
 import fetch from 'isomorphic-unfetch';
 
 const DownloadInput: NextPage = () => {
@@ -26,35 +25,14 @@ const DownloadInput: NextPage = () => {
         return (
             <form
                 ref={form}
-                action="/uploadfile"
+                action={"/upload"}
                 onSubmit={submitHandler}
                 encType="multipart/form-data" method="POST">
                 <input ref={file} onChange={submitHandler} type="file" name="myFile" />
-                <button>go</button>
                 <div>{res}</div>
             </form>
         )
-
     }
-
-    // <div className="input-group">
-    //     <div className="input-group-prepend">
-    // <span className="input-group-text" id="inputGroupFileAddon01">
-    //   Upload
-    // </span>
-    //     </div>
-    //     <div className="custom-file">
-    //         <input
-    //             type="file"
-    //             className="custom-file-input"
-    //             id="inputGroupFile01"
-    //             aria-describedby="inputGroupFileAddon01"
-    //         />
-    //         <label className="custom-file-label" htmlFor="inputGroupFile01">
-    //             Choose file
-    //         </label>
-    //     </div>
-    // </div>
 ;
 
 export default DownloadInput;
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 // call all the required packages
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 const multer = require('multer');
 
-const next = require('next')
-const dev = process.env.NODE_ENV !== 'production'
-const server = next({dev})
-const handle = server.getRequestHandler()
+const next = require('next');
+const dev = process.env.NODE_ENV !== 'production';
+const server = next({dev});
+const handle = server.getRequestHandler();
 
 //CREATE EXPRESS APP
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 // SET STORAGE
@@ -28,34 +28,30 @@ const upload = multer({storage: storage});
 
 server.prepare().then(() => {
 
-
-
 //ROUTES WILL GO HERE
     app.get('/', function (req, res) {
         return server.render(req, res, '/', req.query)
     });
 
-    app.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
-        const file = req.file
+    app.post('/upload', upload.single('myFile'), (req, res, next) => {
+        const file = req.file;
         if (!file) {
-            const error = new Error('Please upload a file')
-            error.httpStatusCode = 400
+            const error = new Error('Please upload a file');
+            error.httpStatusCode = 400;
             return next(error)
         }
         console.log(file);
 
         res.status(200).json({'res': file.originalname+' '+file.size+' symbols'})
 
-    })
+    });
 
     app.all('*', (req, res) => {
         return handle(req, res)
-    })
+    });
 
-
-    app.listen(8080, () => console.log('Server started on port 8080'));
-
-})
+    app.listen(8080, () => console.log(`Server started on http://localhost:8080/`));
+});
 
 
 
